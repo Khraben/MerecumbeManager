40adf97a12b39311fa8d48c3651a45aeec8835e4
Kevin Jiménez <67296979+Khraben@users.noreply.github.com>
2024-10-21 16:15:37 -0600

    Fix: Change Layout Imports

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,6 +1,6 @@
 "use client";
 
-import "../styles/global.css";
+import "@/styles/globals.css";
 import { useState } from "react";
 import { AuthProvider } from "../app/context/AuthContext";
 import SideNavbar from "../app/components/SideNavbar";
