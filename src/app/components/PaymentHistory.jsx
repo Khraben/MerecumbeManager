9568ec2106b1824da8ccfe8c55e2a7206b749094
Kevin Jiménez <kvn.jimenez.t@gmail.com>
2024-11-20 19:54:21 -0600

    Update: Prettier Format Code

--- src/app/components/PaymentHistory.jsx
+++ src/app/components/PaymentHistory.jsx
@@ -1,54 +1,68 @@
-import React, { useState, useEffect } from 'react';
-import styled from 'styled-components';
-import { fetchReceipts, fetchStudentById } from "../firebase/firebaseFirestoreService";
+import React, { useState, useEffect } from "react";
+import styled from "styled-components";
+import {
+  fetchReceipts,
+  fetchStudentById,
+} from "../firebase/firebaseFirestoreService";
 import DatePicker from "react-datepicker";
-import { es } from "date-fns/locale/es"; 
+import { es } from "date-fns/locale/es";
 import "react-datepicker/dist/react-datepicker.css";
-import Loading from "./Loading"; 
-import { FaArrowLeft, FaArrowRight, FaSearch, FaCalendarAlt} from 'react-icons/fa';
+import Loading from "./Loading";
+import {
+  FaArrowLeft,
+  FaArrowRight,
+  FaSearch,
+  FaCalendarAlt,
+  FaTimes,
+} from "react-icons/fa";
 
 const PaymentHistory = ({ onBack }) => {
   const [payments, setPayments] = useState([]);
-  const [selectedStudent, setSelectedStudent] = useState('');
-  const [selectedDetail, setSelectedDetail] = useState(''); 
+  const [selectedStudent, setSelectedStudent] = useState("");
+  const [selectedDetail, setSelectedDetail] = useState("");
   const [startDate, setStartDate] = useState(null);
   const [endDate, setEndDate] = useState(null);
   const [isEndDateDisabled, setIsEndDateDisabled] = useState(true);
   const [filteredPayments, setFilteredPayments] = useState([]);
-  const [loading, setLoading] = useState(true); 
+  const [loading, setLoading] = useState(true);
   const [currentPage, setCurrentPage] = useState(1);
   const paymentsPerPage = 10;
   const maxPageButtons = 4;
 
   useEffect(() => {
     const loadPayments = async () => {
-      setLoading(true); 
+      setLoading(true);
       try {
         const allPayments = await fetchReceipts();
-        const paymentsWithStudentNames = await Promise.all(allPayments.map(async (payment) => {
-          try {
-            const studentData = await fetchStudentById(payment.studentId);
-            return {
-              ...payment,
-              studentName: studentData.name
-            };
-          } catch (error) {
-            console.error(`Error al cargar el estudiante con ID ${payment.studentId}: `, error);
-            return {
-              ...payment,
-              studentName: payment.studentId 
-            };
-          }
-        }));
+        const paymentsWithStudentNames = await Promise.all(
+          allPayments.map(async (payment) => {
+            try {
+              const studentData = await fetchStudentById(payment.studentId);
+              return {
+                ...payment,
+                studentName: studentData.name,
+              };
+            } catch (error) {
+              console.error(
+                `Error al cargar el estudiante con ID ${payment.studentId}: `,
+                error
+              );
+              return {
+                ...payment,
+                studentName: payment.studentId,
+              };
+            }
+          })
+        );
         setPayments(paymentsWithStudentNames);
         setFilteredPayments(paymentsWithStudentNames);
       } catch (error) {
         console.error("Error al cargar los pagos: ", error);
       } finally {
-        setLoading(false); 
+        setLoading(false);
       }
     };
-  
+
     loadPayments();
   }, []);
 
@@ -56,24 +70,39 @@ const PaymentHistory = ({ onBack }) => {
     let filtered = payments;
 
     if (selectedStudent) {
-      filtered = filtered.filter(payment => 
-        payment.studentName && payment.studentName.toLowerCase().includes(selectedStudent.toLowerCase())
+      filtered = filtered.filter(
+        (payment) =>
+          payment.studentName &&
+          payment.studentName
+            .toLowerCase()
+            .includes(selectedStudent.toLowerCase())
       );
     }
-    if (selectedDetail) { 
-      filtered = filtered.filter(payment => 
-        payment.specification && payment.specification.toLowerCase().includes(selectedDetail.toLowerCase())
+    if (selectedDetail) {
+      filtered = filtered.filter(
+        (payment) =>
+          payment.specification &&
+          payment.specification
+            .toLowerCase()
+            .includes(selectedDetail.toLowerCase())
       );
     }
     if (startDate) {
-      filtered = filtered.filter(payment => {
-        const paymentDate = payment.paymentDate && payment.paymentDate.toDate ? payment.paymentDate.toDate() : new Date(payment.paymentDate);
-        return paymentDate >= startDate && (!endDate || paymentDate <= new Date(endDate).setHours(23, 59, 59, 999));
+      filtered = filtered.filter((payment) => {
+        const paymentDate =
+          payment.paymentDate && payment.paymentDate.toDate
+            ? payment.paymentDate.toDate()
+            : new Date(payment.paymentDate);
+        return (
+          paymentDate >= startDate &&
+          (!endDate ||
+            paymentDate <= new Date(endDate).setHours(23, 59, 59, 999))
+        );
       });
     }
     setFilteredPayments(filtered);
     setCurrentPage(1);
-  }, [selectedStudent, selectedDetail, startDate, endDate, payments]); 
+  }, [selectedStudent, selectedDetail, startDate, endDate, payments]);
 
   const handleStartDateChange = (date) => {
     setStartDate(date);
@@ -82,9 +111,31 @@ const PaymentHistory = ({ onBack }) => {
       setEndDate(null);
     }
   };
+
+  const handleClearStartDate = () => {
+    setStartDate(null);
+    setIsEndDateDisabled(true);
+    setEndDate(null);
+  };
+
+  const handleClearEndDate = () => {
+    setEndDate(null);
+  };
+
+  const handleClearStudent = () => {
+    setSelectedStudent("");
+  };
+
+  const handleClearDetail = () => {
+    setSelectedDetail("");
+  };
+
   const indexOfLastPayment = currentPage * paymentsPerPage;
   const indexOfFirstPayment = indexOfLastPayment - paymentsPerPage;
-  const currentPayments = filteredPayments.slice(indexOfFirstPayment, indexOfLastPayment);
+  const currentPayments = filteredPayments.slice(
+    indexOfFirstPayment,
+    indexOfLastPayment
+  );
   const totalPages = Math.ceil(filteredPayments.length / paymentsPerPage);
   const paginate = (pageNumber) => setCurrentPage(pageNumber);
 
@@ -112,15 +163,25 @@ const PaymentHistory = ({ onBack }) => {
             onChange={(e) => setSelectedStudent(e.target.value)}
             placeholder="Filtrar por nombre..."
           />
+          {selectedStudent && (
+            <ClearButton onClick={handleClearStudent}>
+              <FaTimes />
+            </ClearButton>
+          )}
           <SearchIcon />
         </SearchContainer>
         <SearchContainer>
           <SearchInput
             type="text"
-            value={selectedDetail} 
-            onChange={(e) => setSelectedDetail(e.target.value)} 
+            value={selectedDetail}
+            onChange={(e) => setSelectedDetail(e.target.value)}
             placeholder="Filtrar por detalle..."
           />
+          {selectedDetail && (
+            <ClearButton onClick={handleClearDetail}>
+              <FaTimes />
+            </ClearButton>
+          )}
           <SearchIcon />
         </SearchContainer>
         <SearchContainer>
@@ -131,6 +192,11 @@ const PaymentHistory = ({ onBack }) => {
             locale={es}
             placeholderText="Fecha de inicio"
           />
+          {startDate && (
+            <ClearButton onClick={handleClearStartDate}>
+              <FaTimes />
+            </ClearButton>
+          )}
           <CalendarIcon />
         </SearchContainer>
         {startDate && (
@@ -142,39 +208,56 @@ const PaymentHistory = ({ onBack }) => {
               locale={es}
               placeholderText="Fecha de fin"
               disabled={isEndDateDisabled}
+              minDate={startDate}
             />
+            {endDate && (
+              <ClearButton onClick={handleClearEndDate}>
+                <FaTimes />
+              </ClearButton>
+            )}
             <CalendarIcon />
           </SearchContainer>
         )}
       </FilterSection>
       <TableContainer>
-        <PaymentTable>
-          <thead>
-            <tr>
-              <th># Recibo</th>
-              <th>Alumno</th>
-              <th>Fecha</th>
-              <th>Concepto</th>
-              <th>Detalle</th>
-              <th>Monto</th>
-            </tr>
-          </thead>
-          <tbody>
-            {currentPayments.map((payment, index) => (
-              <tr key={index}>
-                <td>{payment.receiptNumber}</td>
-                <td>{payment.studentName}</td>
-                <td>{payment.paymentDate && payment.paymentDate.toDate
-                      ? payment.paymentDate.toDate().toLocaleDateString("es-CR")
-                      : new Date(payment.paymentDate).toLocaleDateString("es-CR")}
-                </td>
-                <td>{payment.concept}</td>
-                <td>{payment.specification ? payment.specification : "Sin detalles"}</td>
-                <td>{payment.amount}</td>
+        {currentPayments.length === 0 ? (
+          <NoDataMessage>No hay pagos registrados en el sistema</NoDataMessage>
+        ) : (
+          <PaymentTable>
+            <thead>
+              <tr>
+                <th># Recibo</th>
+                <th>Alumno</th>
+                <th>Fecha</th>
+                <th>Concepto</th>
+                <th>Detalle</th>
+                <th>Monto</th>
               </tr>
-            ))}
-          </tbody>
-        </PaymentTable>
+            </thead>
+            <tbody>
+              {currentPayments.map((payment, index) => (
+                <tr key={index}>
+                  <td>{payment.receiptNumber}</td>
+                  <td>{payment.studentName}</td>
+                  <td>
+                    {payment.paymentDate && payment.paymentDate.toDate
+                      ? payment.paymentDate.toDate().toLocaleDateString("es-CR")
+                      : new Date(payment.paymentDate).toLocaleDateString(
+                          "es-CR"
+                        )}
+                  </td>
+                  <td>{payment.concept}</td>
+                  <td>
+                    {payment.specification
+                      ? payment.specification
+                      : "Sin detalles"}
+                  </td>
+                  <td>{payment.amount}</td>
+                </tr>
+              ))}
+            </tbody>
+          </PaymentTable>
+        )}
       </TableContainer>
       <Pagination>
         {currentPage > 1 && (
@@ -183,7 +266,11 @@ const PaymentHistory = ({ onBack }) => {
           </PageIcon>
         )}
         {getPageNumbers().map((page) => (
-          <PageButton key={page} onClick={() => paginate(page)} active={page === currentPage}>
+          <PageButton
+            key={page}
+            onClick={() => paginate(page)}
+            active={page === currentPage}
+          >
             {page}
           </PageButton>
         ))}
@@ -196,7 +283,15 @@ const PaymentHistory = ({ onBack }) => {
       <BackButton onClick={onBack}>Volver</BackButton>
     </Wrapper>
   );
-}
+};
+
+const NoDataMessage = styled.p`
+  font-size: 18px;
+  color: #333;
+  text-align: center;
+  margin-top: 20px;
+`;
+
 const Wrapper = styled.div`
   width: 100%;
   display: flex;
@@ -208,6 +303,7 @@ const Wrapper = styled.div`
     padding: 1px;
   }
 `;
+
 const Title = styled.h1`
   font-size: 24px;
   color: #0b0f8b;
@@ -221,6 +317,7 @@ const Title = styled.h1`
     margin-bottom: 10px;
   }
 `;
+
 const FilterSection = styled.div`
   margin-bottom: 20px;
   display: flex;
@@ -232,6 +329,7 @@ const FilterSection = styled.div`
     align-items: center;
   }
 `;
+
 const TableContainer = styled.div`
   width: 100%;
   padding: 0 20px;
@@ -244,13 +342,14 @@ const TableContainer = styled.div`
     padding: 0 10px;
   }
 `;
+
 const PaymentTable = styled.table`
   width: 100%;
   max-width: 1200px;
   border-collapse: collapse;
   background-color: transparent;
   border-radius: 8px;
- 
+
   thead {
     position: sticky;
     top: 0;
@@ -258,7 +357,8 @@ const PaymentTable = styled.table`
     color: #dddddd;
   }
 
-  th, td {
+  th,
+  td {
     padding: 16px 20px;
     text-align: left;
     border-bottom: 1px solid #ddd;
@@ -281,7 +381,8 @@ const PaymentTable = styled.table`
   }
 
   @media (max-width: 768px) {
-    th, td {
+    th,
+    td {
       font-size: 12px;
       padding: 12px 15px;
     }
@@ -289,13 +390,15 @@ const PaymentTable = styled.table`
 
   @media (max-width: 480px) {
     margin-left: 160px;
-    
-    th, td {
+
+    th,
+    td {
       font-size: 10px;
       padding: 10px 12px;
     }
   }
 `;
+
 const Pagination = styled.div`
   display: flex;
   justify-content: center;
@@ -305,13 +408,14 @@ const Pagination = styled.div`
     margin-top: 10px;
   }
 `;
+
 const PageButton = styled.button`
   padding: 10px 15px;
   margin: 0 5px;
   font-size: 14px;
   font-weight: bold;
   color: #dddddd;
-  background-color: ${props => props.active ? '#073e8a' : '#0b0f8b'};
+  background-color: ${(props) => (props.active ? "#073e8a" : "#0b0f8b")};
   border: none;
   border-radius: 5px;
   cursor: pointer;
@@ -330,6 +434,7 @@ const PageButton = styled.button`
     font-size: 12px;
   }
 `;
+
 const PageIcon = styled.div`
   padding: 10px 15px;
   margin: 0 5px;
@@ -352,6 +457,7 @@ const PageIcon = styled.div`
     font-size: 12px;
   }
 `;
+
 const BackButton = styled.button`
   padding: 10px 20px;
   margin-top: 20px;
@@ -378,6 +484,7 @@ const BackButton = styled.button`
     margin-top: 10px;
   }
 `;
+
 const StyledDatePicker = styled(DatePicker)`
   width: 100%;
   padding: 10px 15px;
@@ -386,12 +493,13 @@ const StyledDatePicker = styled(DatePicker)`
   border-radius: 5px;
   background-color: transparent;
   z-index: 5;
-  position: relative; 
+  position: relative;
   @media (max-width: 480px) {
     padding: 8px 12px;
     font-size: 12px;
   }
 `;
+
 const SearchContainer = styled.div`
   display: flex;
   align-items: center;
@@ -408,6 +516,7 @@ const SearchContainer = styled.div`
     margin-bottom: 10px;
   }
 `;
+
 const SearchInput = styled.input`
   width: 100%;
   padding: 10px 40px 10px 15px;
@@ -418,32 +527,52 @@ const SearchInput = styled.input`
   background-color: transparent;
 
   @media (max-width: 480px) {
-    padding: 8px 35px 8px 12px; 
+    padding: 8px 35px 8px 12px;
     font-size: 12px;
   }
 `;
+
 const SearchIcon = styled(FaSearch)`
   position: absolute;
-  right: 30px; 
+  right: 30px;
   color: #0b0f8b;
   font-size: 18px;
   cursor: pointer;
 
   @media (max-width: 480px) {
-    right: 25px; 
+    right: 25px;
     font-size: 16px;
   }
 `;
+
 const CalendarIcon = styled(FaCalendarAlt)`
   position: absolute;
-  right: 30px; 
+  right: 30px;
   color: #0b0f8b;
   font-size: 18px;
   cursor: pointer;
 
   @media (max-width: 480px) {
-    right: 25px; 
+    right: 25px;
     font-size: 16px;
   }
 `;
+
+const ClearButton = styled.button`
+  position: absolute;
+  right: 50px;
+  top: 10px;
+  background: none;
+  border: none;
+  color: #0b0f8b;
+  font-size: 18px;
+  cursor: pointer;
+  z-index: 1001;
+
+  @media (max-width: 480px) {
+    right: 45px;
+    font-size: 16px;
+  }
+`;
+
 export default PaymentHistory;
